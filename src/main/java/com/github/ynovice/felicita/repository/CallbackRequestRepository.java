5be832c5467648b03c424c52fd36bc956939154d
ynovice <catgraywhite@gmail.com>
2023-10-07 20:50:07 +0300

    Get rid of callback requests feature

--- src/main/java/com/github/ynovice/felicita/repository/CallbackRequestRepository.java
+++
@@ -1,6 +0,0 @@
-package com.github.ynovice.felicita.repository;
-
-import com.github.ynovice.felicita.model.entity.CallbackRequest;
-import org.springframework.data.jpa.repository.JpaRepository;
-
-public interface CallbackRequestRepository extends JpaRepository<CallbackRequest, Long> {}
---
+++ src/main/resources/db/changelog/v.2.0/10-rollback.sql
@@ -0,0 +1,7 @@
+create table callback_requests
+(
+    id         bigserial primary key,
+    created_at timestamp(6),
+    name       varchar(255),
+    phone      varchar(255)
+);
